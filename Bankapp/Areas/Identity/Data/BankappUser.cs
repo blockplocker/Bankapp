43fe07d2ea45b32003cc8f8e590e29c20b62e5a7
blockplocker <noahallklint@gmail.com>
2025-11-21 15:54:28 +0100

    Integrate ASP.NET Core Identity into Bankapp
    Added ASP.NET Core Identity for user authentication, registration, and account management. Configured Entity Framework Core with SQL Server

---
+++ Bankapp/Areas/Identity/Data/BankappUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bankapp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the BankappUser class
+public class BankappUser : IdentityUser
+{
+}
+
