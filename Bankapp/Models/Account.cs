74ba35263c8f0ac355901939d3f76ddb8bd592da
blockplocker <noahallklint@gmail.com>
2025-12-01 15:09:56 +0100

    use primary construktor in transaction and account and simplified amount validation

--- Bankapp/Models/Account.cs
+++ Bankapp/Models/Account.cs
@@ -1,17 +1,17 @@
 ﻿using Bankapp.Areas.Identity.Data;
-using Microsoft.AspNetCore.Identity;
 
 namespace Bankapp.Models
 {
-    public class Account
+    public class Account(string accountName, decimal balance, int accountNumber, string userId, ICollection<Transaction> transactions)
     {
         public int AccountId { get; set; }
-        public string AccountName { get; set; }
-        public decimal Balance { get; set; }
-        public int AccountNumber { get; set; }
+        public string AccountName { get; set; } = accountName;
+        public decimal Balance { get; set; } = balance;
+        public int AccountNumber { get; set; } = accountNumber;
 
-        public string UserId { get; set; }
+
+        public string UserId { get; set; } = userId;
         public BankappUser User { get; set; }
-        public ICollection<Transaction> Transactions { get; set; }
+        public ICollection<Transaction> Transactions { get; set; } = transactions;
     }
 }
--- Bankapp/Models/Transaction.cs
+++ Bankapp/Models/Transaction.cs
@@ -1,15 +1,15 @@
-﻿namespace Bankapp.Models
+﻿
+namespace Bankapp.Models
 {
-    public class Transaction
+    public class Transaction(int accountId, decimal amount, DateTime date, TransactionType type)
     {
         public int Id { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
-        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; } = amount;
+        public DateTime Date { get; set; } = date;
+        public TransactionType Type { get; set; } = type;
 
 
-
-        public int AccountId { get; set; }
+        public int AccountId { get; set; } = accountId;
         public Account Account { get; set; }
     }
 }
